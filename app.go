5e10481c6d746b04e002d13d532caa48f6eb8694
jamfly <cocoredju222@gmail.com>
2019-06-26 14:31:37 +0800

    Init project

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module tat_gogogo
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+)
