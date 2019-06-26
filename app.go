a2175f70956d54b39370d8cd07c45539bfb56e80
jamfly <cocoredju222@gmail.com>
2019-06-26 19:50:12 +0800

    Init login api

--- app.go
+++ app.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"tat_gogogo/consts"
 
 	"github.com/gin-gonic/gin"
 )
 
+func login(studentID string, password string) {
+
+	res, err := http.PostForm(consts.Login,
+		url.Values{"forceMobile": {"mobile"}, "mpassword": {password}, "muid": {studentID}})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	res.Header.Set("Referer", consts.IndexPage)
+	res.Header.Set("User-Agent", "Direk Android App")
+
+	log.Println(res.Cookies())
+}
+
 func main() {
-	fmt.Println("hello world")
+
+	log.Println(consts.Base)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.POST("/login", func(c *gin.Context) {
+		studentID := c.PostForm("studentID")
+		password := c.PostForm("password")
+
+		login(studentID, password)
+
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"status": "posted",
+			"muid":   studentID,
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
