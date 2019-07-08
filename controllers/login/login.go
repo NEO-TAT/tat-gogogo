76e224a34b47469e0c571f1da442ca9191be4821
jamfly <cocoredju222@gmail.com>
2019-07-08 23:58:13 +0800

    Fix request cookie issue

--- controllers/login/login.go
+++ controllers/login/login.go
@@ -3,10 +3,18 @@ package login
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"net/http/cookieJar"
 	"net/url"
 	"tat_gogogo/consts"
 	"log"
+	"bytes"
+	//"io/ioutil"
 )
+
+type Result struct {
+	success bool
+	status int
+}
 
 func LoginController(c *gin.Context) {
 	studentId := c.PostForm("studentId")
@@ -20,17 +28,49 @@ func LoginController(c *gin.Context) {
 	})
 }
 
-func login(studentID string, password string) {
+func newClient(studentID string, password string) (*http.Client, *http.Request) {
+	cookieJar, _ := cookiejar.New(nil)
+	
+	client := &http.Client{
+		Jar: cookieJar,
+	}
 
-	res, err := http.PostForm(consts.Login,
-		url.Values{"forceMobile": {"mobile"}, "mpassword": {password}, "muid": {studentID}})
+	data := 	url.Values{
+		"forceMobile": {"mobile"},
+		"mpassword": {password}, 
+		"muid": {studentID},
+	}
+
+	req, err := http.NewRequest("POST", consts.Login, bytes.NewBufferString(data.Encode()))
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	res.Header.Set("Referer", consts.IndexPage)
-	res.Header.Set("User-Agent", "Direk Android App")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Referer", consts.IndexPage)
+	req.Header.Set("User-Agent", "Direk Android App")
 
-	log.Println(res.Cookies())
+	return client, req
+}
+
+func handleRequest(studentID string, password string) (Result) {
+	client, req := newClient(studentID, password)
+	resp, err := client.Do(req)
+	
+	if err != nil {
+		log.Fatalln(err)
+		return Result{success: false, status: 401}
+	}
+
+	defer resp.Body.Close()
+
+	return Result{success: true, status: 200}
+}
+
+func login(studentID string, password string) {
+
+	result := handleRequest(studentID, password)
+
+	log.Println(result)
 }
