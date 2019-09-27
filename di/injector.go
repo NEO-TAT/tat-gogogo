6ac6e1724233af8ca824b1343ee06e67b99ec260
jamfly <cocoredju222@gmail.com>
2019-09-27 20:05:30 +0800

    Add DI via wire

---
+++ di/injector.go
@@ -0,0 +1,74 @@
+//+build wireinject
+
+package di
+
+import (
+	"tat_gogogo/data/crawler/repository"
+	"tat_gogogo/data/crawler/service"
+	"tat_gogogo/interface/controller"
+	"tat_gogogo/usecase"
+
+	"github.com/google/wire"
+)
+
+var provideCurriculumRepo = wire.NewSet(repository.NewCurriculumRepository)
+var provideResultRepo = wire.NewSet(repository.NewResultRepository)
+var provideInfoRepo = wire.NewSet(repository.NewInfoRepository)
+
+var provideCurriculumService = wire.NewSet(service.NewCurriculumService)
+var provideResultService = wire.NewSet(service.NewResultService)
+var provideInfoService = wire.NewSet(service.NewInfoService)
+
+var provideCurriculumController = wire.NewSet(
+	provideCurriculumRepo,
+	provideResultRepo,
+	provideInfoRepo,
+	provideResultService,
+	usecase.NewResultUseCase,
+	controller.NewCurriculumController,
+)
+
+var provideCoursesController = wire.NewSet(
+	provideCurriculumRepo,
+	provideResultRepo,
+	provideInfoRepo,
+	provideResultService,
+	provideCurriculumService,
+	usecase.NewResultUseCase,
+	usecase.NewCurriculumUseCase,
+	controller.NewCoursesController,
+)
+var provideLoginController = wire.NewSet(
+	provideCurriculumRepo,
+	provideResultRepo,
+	provideInfoRepo,
+	provideResultService,
+	usecase.NewResultUseCase,
+	provideCurriculumService,
+	usecase.NewCurriculumUseCase,
+	controller.NewLoginController,
+)
+
+/*
+InjectCourseController is a function which will inject all dependencies
+*/
+func InjectCourseController() controller.CoursesController {
+	wire.Build(provideCoursesController)
+	return nil
+}
+
+/*
+InjectCurriculumController is a function which will inject all dependencies
+*/
+func InjectCurriculumController() controller.CurriculumController {
+	wire.Build(provideCurriculumController)
+	return nil
+}
+
+/*
+InjectLoginController is a function which will inject all dependencies
+*/
+func InjectLoginController() controller.LoginController {
+	wire.Build(provideLoginController)
+	return nil
+}
